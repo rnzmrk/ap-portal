3925a59ad367f4e5ddad890ecc52e5044ffb6c59
rnzmrk <olearenzemark@gmail.com>
2026-06-21 22:51:50 +0800

    "test.03"

--- app/Http/Requests/PoGppo/StorePoGppoRequest.php
+++ app/Http/Requests/PoGppo/StorePoGppoRequest.php
@@ -57,9 +57,13 @@ class StorePoGppoRequest extends FormRequest
             'raw_post' => $_POST,
             'raw_files' => $filesInfo,
             'php_ini' => [
+                'file_uploads' => ini_get('file_uploads'),
                 'upload_max_filesize' => ini_get('upload_max_filesize'),
                 'post_max_size' => ini_get('post_max_size'),
                 'max_file_uploads' => ini_get('max_file_uploads'),
+                'upload_tmp_dir' => ini_get('upload_tmp_dir'),
+                'sys_temp_dir' => sys_get_temp_dir(),
+                'is_sys_temp_writable' => is_writable(sys_get_temp_dir()),
             ]
         ]);
     }
